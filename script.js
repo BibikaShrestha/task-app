32465d4cb9deac6ce9a1f115ba6746363a69d40c
BibikaShrestha <np03cs4a240020@heraldcollege.edu.np>
2025-07-23 07:40:07 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+var age = 25;
+var age = 24;
+console.log(age);
+
+let name = "Jenny";
+name = "Jeshmi";
+console.log(name);
+
+const address = "Jhamsikhel";
+console.log(typeof address);
+
+const student = {
+  fullName: "Jenny Rajak",
+  age: 30,
+  address: "Naxal",
+  phoneNumber: "98000000",
+};
+console.log(student);
+
+const attendance = {
+  studentID: "NP03CS4A240304",
+  studentName: "Anup Limbu",
+  isPresent: true,
+};
+console.log;
